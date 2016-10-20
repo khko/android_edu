e2e751aeb90b8850945ff632e4285018fc675edd
androideduTester <kyunghyun.ko@m3solutions.co.kr>
2016-10-21 00:56:01 +0900

    [강사]
    메인 페이지 버튼 추가

--- MyDiary/app/src/main/java/com/androidedu/example/mydiary/MainActivity.java
+++ MyDiary/app/src/main/java/com/androidedu/example/mydiary/MainActivity.java
@@ -1,7 +1,8 @@
 package com.androidedu.example.mydiary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // git hub 테스트
+    }
+
+    //R.id.btnCategoryAddDiaryListActivity
+    public void onClickedBtnCategoryAddDiaryListStart(View v)
+    {
+
+    }
+
+    //R.id.btnCategoryDiaryListStart
+    public void onClickedBtnCategoryDiaryListStart(View v)
+    {
+
+    }
+
+    //R.id.btnCategoryListStart
+    public void onClickedBtnCategoryListStart(View v)
+    {
+
+    }
+
+    //R.id.btnDiaryEditStart
+    public void onClickedBtnDiaryEditStart(View v)
+    {
+
+    }
+
+    //R.id.btnDiaryInsertStart
+    public void onClickedBtnDiaryInsertStart(View v)
+    {
+
+    }
+
+    //R.id.btnDiaryListStart
+    public void onClickedBtnDiaryListStart(View v)
+    {
+
+    }
+
+    //R.id.btnDiaryViewStart
+    public void onClickedBtnDiaryViewStart(View v)
+    {
+
+    }
+
+    //R.id.btnIntroStart
+    public void onClickedBtnIntroStart(View v)
+    {
+
+    }
+
+    //R.id.btnPopupCategoryInsertStart
+    public void onClickedBtnPopupCategoryInsertStart(View v)
+    {
+
+    }
+
+    //R.id.btnPopupCategoryStart
+    public void onClickedBtnPopupCategoryStart(View v)
+    {
+
     }
 }
